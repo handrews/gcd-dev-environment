b477e00aeb270feaf0defb133bddfc27f9a255a2
Henry Andrews <andrews_henry@yahoo.com>
2019-11-20 14:02:30 -0800

    Source virtualenv

--- provision-python.sh
+++ provision-python.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 python3 -m venv /opt/venv && \
+echo "source /opt/venv/bin/activate" >> /home/vagrant/.bash_profile && \
 source /opt/venv/bin/activate && \
 pip install -U pip && \
 pip install -r /vagrant/www/requirements.txt
